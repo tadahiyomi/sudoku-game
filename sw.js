d6d6192cc7bec6df1d04f3903f00a2575b69c3f7
tadahiyomi <a0930669@gmail.com>
2026-05-22 01:59:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mini-games-v3";
+const CACHE_NAME = "mini-games-v4";
 const ASSETS = ["./", "./index.html", "./sudoku.html", "./minesweeper.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
@@ -11,5 +11,13 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
 });
